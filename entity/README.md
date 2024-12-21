4395e1cb55ea4167c577ba8e5e09d0e7cc4693f1
Felipe Fause <felipe.fause@gmail.com>
2024-12-20 22:03:25 -0300

    Adicionando o pacote contendo dados de entidades

---
+++ entity/README.md
@@ -0,0 +1,1 @@
+"Entity" pode ser usado informalmente para se referir a qualquer objeto ou classe que represente algo concreto dentro de uma aplicação. Por exemplo, um objeto que modela um cliente, produto ou pedido.
---
+++ entity/cadastro_entity.py
@@ -0,0 +1,9 @@
+class Cadastro:
+    # Define a classe Cadastro, que representa a entidade principal do sistema.
+    def __init__(self, name: str, email: str, phone: str, birth_date: str, addresses: list):
+        # Inicializa os atributos da entidade com os dados do cadastro.
+        self.name = name  # Nome do usuário cadastrado.
+        self.email = email  # Email do usuário cadastrado.
+        self.phone = phone  # Telefone do usuário cadastrado.
+        self.birth_date = birth_date  # Data de nascimento do usuário cadastrado.
+        self.addresses = addresses  # Lista de endereços associados ao cadastro.
