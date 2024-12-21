ede649d0e11ff4ad9feceec757845fb2e7dea01e
Felipe Fause <felipe.fause@gmail.com>
2024-12-20 22:06:12 -0300

    Adicionando o pacote contendo dados de servicos

---
+++ service/cadastro_service.py
@@ -0,0 +1,12 @@
+class CadastroService:
+    # Define a interface para operações de serviço relacionadas a cadastros.
+    def create_cadastro(self, cadastro_dto):
+        pass  # verificar com o Gerente
+    def get_all_cadastros(self):
+        pass  # verificar com o Gerente
+
+    def update_cadastro(self, email, cadastro_dto):
+        pass  # verificar com o Gerente
+
+    def delete_cadastro(self, email):
+        pass  # verificar com o Gerente
---
+++ service/service_implementation/cadastro_service_implementation.py
@@ -0,0 +1,38 @@
+from repository.cadastro_repository import CadastroRepository
+from entity.cadastro_entity import Cadastro
+
+class CadastroServiceImplementation:
+    # Implementa os métodos definidos na interface CadastroService.
+    def __init__(self):
+        self.repository = CadastroRepository()  # Instancia o repositório.
+
+    def create_cadastro(self, cadastro_dto):
+        # Cria um novo cadastro a partir do DTO e salva no repositório.
+        cadastro = Cadastro(
+            name=cadastro_dto.name,
+            email=cadastro_dto.email,
+            phone=cadastro_dto.phone,
+            birth_date=cadastro_dto.birth_date,
+            addresses=cadastro_dto.addresses
+        )
+        self.repository.save(cadastro)
+
+    def get_all_cadastros(self):
+        # Retorna todos os cadastros armazenados no repositório.
+        return self.repository.find_all()
+
+    def update_cadastro(self, email, cadastro_dto):
+        # Atualiza os dados de um cadastro existente com base no email.
+        updated_cadastro = Cadastro(
+            name=cadastro_dto.name,
+            email=cadastro_dto.email,
+            phone=cadastro_dto.phone,
+            birth_date=cadastro_dto.birth_date,
+            addresses=cadastro_dto.addresses
+        )
+        return self.repository.update(email, updated_cadastro)
+
+    def delete_cadastro(self, email):
+        # Remove um cadastro do repositório com base no email.
+        # ( a escolha do e-mail foi pelo fato de que varias pessoas podem ter nomes iguais)
+        return self.repository.delete(email)
