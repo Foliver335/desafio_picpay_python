78bb6eaad7928383757634f2ed660f6dcf96e096
Felipe Fause <felipe.fause@gmail.com>
2024-12-21 08:10:15 -0300

    modificação: inserção da variavel 'nickname' para criar um id unico

--- dto/cadastro_dto.py
+++ dto/cadastro_dto.py
@@ -1,8 +1,9 @@
 class CadastroDTO:
     # Define a classe DTO (Data Transfer Object) para transferir dados entre camadas.
     
-    def __init__(self, name: str, email: str, phone: str, birth_date: str, addresses: list):
+    def __init__(self,nickname: str, name: str, email: str, phone: str, birth_date: str, addresses: list):
         # Inicializa os atributos do DTO com os dados do cadastro.
+        self.nickname = nickname
         self.name = name
         self.email = email
         self.phone = phone
@@ -12,6 +13,7 @@ class CadastroDTO:
     def to_dict(self):
         # Converte os dados do DTO para um dicionário, facilitando a serialização em JSON.
         return {
+            'nickname': self.nickname,
             'name': self.name,
             'email': self.email,
             'phone': self.phone,
